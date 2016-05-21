5cea6861b85524df51f9f83593d0769f11fcb177
Jimenez <octavio.jimenez@hpe.com>
2016-05-21 13:58:37 -0600

    set env port

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var photo = require("nat-geo-api");
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var env_port = process.env.PORT || 8080;
+
 photo.run({
-  port: 5000
+  port: env_port
 });
